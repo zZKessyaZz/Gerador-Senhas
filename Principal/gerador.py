5df6e10695f178c721e2aa29879b2907cbc7ec3f
Késsya Cavalcanti <kessyacavalcanti2017@gmail.com>
2025-08-04 15:00:26 -0300

    Construção inicial da janela tkinter do gerador, ainda precisa de ajustes

--- Principal/gerador.py
+++ Principal/gerador.py
@@ -1,6 +1,35 @@
-#Estrutura da interface:
+import tkinter as tk
 
-#- Campo para definir o tamanho da senha (Entry ou Spinbox)
-#- Botão "Gerar senha"
-#- Campo para exibir a senha gerada
-#- Botão "Copiar senha"
+#Janela
+janela = tk.Tk()
+janela.title("Gerador de Senhas")
+janela.geometry("500x300")
+janela.config(bg = "lightgray")
+
+#Frame
+botoes_frame = tk.Frame(janela, bg = "lightgray")
+botoes_frame.place(relx=0.5, rely=0.35, anchor="center")
+
+
+btn = tk.Button(botoes_frame, text="Gerar Senha")
+btn.pack(side = "left", padx=10)
+btn1 = tk.Button(botoes_frame, text="Copiar senha")
+btn1.pack(side = "left", padx=10)
+
+#Elementos
+tamanho_senha = tk.Spinbox(janela)
+tamanho_senha.pack()
+
+campo_senha = tk.Entry()
+campo_senha.pack()
+
+
+
+
+
+
+
+
+
+
+janela.mainloop()
