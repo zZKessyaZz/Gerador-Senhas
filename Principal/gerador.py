b1ecad406c6dedc430314415eccca7a843ffe312
Késsya Cavalcanti <kessyacavalcanti2017@gmail.com>
2025-08-05 17:37:46 -0300

    Lógica da geração de senhas, copiar para área de transferência e mensagens de confirmação adicionadas.

--- Principal/gerador.py
+++ Principal/gerador.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import string
+import random
 
 #Janela
 janela = tk.Tk()
@@ -22,24 +24,48 @@ foto1 = ImageTk.PhotoImage(imagem_tamanho)
 label_imagem = tk.Label(frame_principal, image = foto1, bg = "lightblue")
 label_imagem.pack(pady = 10)
 
+
+#Lógica para gerar e copiar a senha
+
+def gerar_senha():
+    tamanho = int(tamanho_senha.get())
+    caracteres = string.ascii_letters + string.digits + "!@#$%&*"
+    senha = "".join(random.choice(caracteres) for _ in range(tamanho))
+    campo_senha.delete(0, tk.END)
+    campo_senha.insert(0, senha)
+
+def copiar_senha():
+    senha_gerada = campo_senha.get()
+    if senha_gerada:
+        janela.clipboard_clear()
+        janela.clipboard_append(senha_gerada)
+        janela.update()
+
+        label_confirmacao = tk.Label(frame_copiar, text="Senha Copiada!",bg="lightgray", fg="green")
+        label_confirmacao.pack(pady = 5)
+        janela.after(2000, label_confirmacao.destroy)
+    else:
+        label_confirmacao = tk.Label(frame_copiar, text="Nada pra copiar!", bg= "lightgray", fg="red")
+        label_confirmacao.pack(pady=5)
+        janela.after(2000, label_confirmacao.destroy)
+
+
 #Frame gerar senha
 frame_senha = tk.Frame(frame_principal, bg="lightgray")
 frame_senha.pack(pady=10)
 label_senha = tk.Label(frame_senha, text="Escolha a quantidade de dígitos da senha:",font=("Comic Sans Ms",12), bg="lightgray")
 label_senha.pack(pady = 5)
-tamanho_senha = tk.Spinbox(frame_senha)
+tamanho_senha = tk.Spinbox(frame_senha, from_= 4, to=16, justify="center")
 tamanho_senha.pack(side = "left", padx=5)
-btn = tk.Button(frame_senha, text="Gerar Senha")
+btn = tk.Button(frame_senha, text="Gerar Senha", command= gerar_senha)
 btn.pack(side = "left", padx=5)
 
 #Frame copiar senha
 frame_copiar = tk.Frame(frame_principal, bg = "lightgray")
 frame_copiar.pack(pady=10)
-campo_senha = tk.Entry(frame_copiar)
+campo_senha = tk.Entry(frame_copiar,justify="center")
 campo_senha.pack(pady=5)
-btn1 = tk.Button(frame_copiar, text="Copiar Senha")
-btn1.pack(pady=5)
-
-
+btn1 = tk.Button(frame_copiar, text="Copiar Senha", command=copiar_senha)
+btn1.pack(pady=5, padx=10)
 
 janela.mainloop()
