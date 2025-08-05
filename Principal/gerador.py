d9ac0fecc8f29bc97172980e6ccf00523ddf107d
Késsya Cavalcanti <kessyacavalcanti2017@gmail.com>
2025-08-05 09:43:02 -0300

    Organizei a interface e adicionei imagem no repositório

--- Principal/gerador.py
+++ Principal/gerador.py
@@ -2,33 +2,33 @@ import tkinter as tk
 
 #Janela
 janela = tk.Tk()
-janela.title("Gerador de Senhas")
-janela.geometry("500x300")
+janela.title(" ")
+janela.geometry("600x400")
 janela.config(bg = "lightgray")
+janela.minsize(width=600, height= 400)
+janela.maxsize(width=600, height=400)
 
-#Frame
-botoes_frame = tk.Frame(janela, bg = "lightgray")
-botoes_frame.place(relx=0.5, rely=0.35, anchor="center")
+#Frame principal
+frame_principal = tk.Frame(janela, bg="lightgray")
+frame_principal.pack(expand=True)
 
+#Frame gerar senha
+frame_senha = tk.Frame(frame_principal, bg="lightgray")
+frame_senha.pack(pady=10)
+label_senha = tk.Label(frame_senha, text="Escolha a quantidade de dígitos da senha:", bg="gray")
+label_senha.pack(pady = 5)
+tamanho_senha = tk.Spinbox(frame_senha)
+tamanho_senha.pack(side = "left", padx=5)
+btn = tk.Button(frame_senha, text="Gerar Senha")
+btn.pack(side = "left", padx=5)
 
-btn = tk.Button(botoes_frame, text="Gerar Senha")
-btn.pack(side = "left", padx=10)
-btn1 = tk.Button(botoes_frame, text="Copiar senha")
-btn1.pack(side = "left", padx=10)
-
-#Elementos
-tamanho_senha = tk.Spinbox(janela)
-tamanho_senha.pack()
-
-campo_senha = tk.Entry()
-campo_senha.pack()
-
-
-
-
-
-
-
+#Frame copiar senha
+frame_copiar = tk.Frame(frame_principal, bg = "lightgray")
+frame_copiar.pack(pady=10)
+campo_senha = tk.Entry(frame_copiar)
+campo_senha.pack(pady=5)
+btn1 = tk.Button(frame_copiar, text="Copiar Senha")
+btn1.pack(pady=5)
 
 
 
